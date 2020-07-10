213bf0001927c5f8921486a591bcfff7f485c295
송경용 <dragon@DRAGON.local>
2020-07-10 12:46:54 +0900

    Django draft

---
+++ pybo/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from pybo.models import Question, Answer
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['subject', 'content']
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
---
+++ pybo/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
+from .forms import QuestionForm, AnswerForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.core.paginator import Paginator
+
+def index(request):
+    #return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
+    # 조회
+    question_list = Question.objects.order_by('-create_date')
+
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
