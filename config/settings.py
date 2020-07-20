c7de81bbc037af343427ef599148b016dbc59720
SongKyeongYong <ruuddyd@gmail.com>
2020-07-20 14:53:42 +0900

    ALLOWD_HOST

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+ALLOWED_HOSTS = ['13.124.63.245']
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
